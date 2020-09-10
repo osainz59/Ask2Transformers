06d189f2477e9822f4b2086500f87c2ed1ebf68f
Oscar Sainz <osainz59@hotmail.com>
2020-09-10 16:10:30 +0200

    Add version information

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+import a2t
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="a2t",
-    version="0.1",
+    version=a2t.__version__,
     author="Oscar Sainz",
     author_email="osainz006@ehu.eus",
     description="Ask2Transformers is a library for zero-shot classification based on Transformers.",
