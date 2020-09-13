a23bf22989687c776aa4451f78cc568657e36dda
Oscar Sainz <osainz59@gmail.com>
2020-09-13 17:57:58 +0200

    Testing with WNDomains

--- scripts/align_glosses_with_labels.py
+++ scripts/align_glosses_with_labels.py
@@ -2,16 +2,25 @@ import gzip
 import sys
 
 if len(sys.argv) < 3:
-    print("ERROR. Usage: python3 align_glosses_with_labels.py labels.txt glosses.txt")
+    print("ERROR. Usage: python3 align_glosses_with_labels.py labels.txt glosses.txt [babel/wndomains]")
+
+if len(sys.argv) == 4:
+    assert sys.argv[3] in ['babel', 'wndomains']
+    label_type = 1 if sys.argv[3] == 'babel' else 2
+else:
+    label_type = 1
+
 
 with open(sys.argv[1], 'rt') as f:
     labels = {}
     for line in f:
-        row = line.split()
-        idx, label = row[0], " ".join(row[1:])
+        row = line.strip().split('\t')
+        idx, label = row[0], row[label_type]
+        if label == 'None':
+            continue
         labels[idx] = label
 
-with gzip.open(sys.argv[2], 'rt') as f:
+with gzip.open(sys.argv[2], 'rt', encoding='utf-8') as f:
     for line in f:
         row = line.split()
         idx, gloss = row[0], " ".join(row[1:])
